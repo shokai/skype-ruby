1a76e668f22d5eae034f6b11ca8431134f88b029
Sho Hashimoto <hashimoto@shokai.org>
2013-06-20 07:01:49 +0900

    set app_name in platforms/mac

--- lib/skype/platforms/mac.rb
+++ lib/skype/platforms/mac.rb
@@ -2,6 +2,6 @@ require "appscript"
 
 module Skype
   def self.exec(command)
-    Appscript.app("skype").send_ :script_name => "", :command => command
+    Appscript.app("skype").send_ :script_name => self.opts[:app_name], :command => command
   end
 end
