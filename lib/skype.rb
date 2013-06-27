8dffab9389791c551e25d595703aa25041549d47
Sho Hashimoto <hashimoto@shokai.org>
2013-06-27 09:27:35 +0900

    move platform load code into skype.rb

--- lib/skype.rb
+++ lib/skype.rb
@@ -1,6 +1,16 @@
 require "rubygems"
 
 require "skype/version"
+
+require case RUBY_PLATFORM
+        when /darwin/
+          "skype/platforms/mac"
+        when /linux/
+          "skype/platforms/linux"
+        else
+          STDERR.puts %Q{!!Skype gem cannot support your platform "#{RUBY_PLATFORM}" not support.}
+        end
+
 require "skype/main"
 
 module Skype
--- lib/skype/main.rb
+++ lib/skype/main.rb
@@ -1,12 +1,3 @@
-require case RUBY_PLATFORM
-        when /darwin/
-          "skype/platforms/mac"
-        when /linux/
-          "skype/platforms/linux"
-        else
-          STDERR.puts %Q{!!Skype gem cannot support your platform "#{RUBY_PLATFORM}" not support.}
-        end
-
 module Skype
   @@config = {:app_name => "ruby-skype"}
   def self.config(conf=nil)
