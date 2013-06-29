6847025bf73d0b33e294357f1712f4abfdc318f7
Sho Hashimoto <hashimoto@shokai.org>
2013-06-29 14:01:56 +0900

    escape for osascript #7

--- lib/skype/platforms/mac.rb
+++ lib/skype/platforms/mac.rb
@@ -1,10 +1,17 @@
 module Skype
   def self.exec(command)
     script = %Q{tell application "Skype"
-  send command "#{command}" script name "#{self.config[:app_name]}"
+  send command "#{Utils.escape command}" script name "#{self.config[:app_name]}"
 end tell}
     res = `/usr/bin/osascript -e '#{script}'`.split(/[\n\r]+/)
     res.shift if res.size > 1 and res[0] =~ /^dyld: /
     res[0]
+  end
+
+  module Utils
+    def self.escape(str)
+      str.gsub(/(["\\])/){ "\\#{$1}" }.
+        gsub("'"){ "'\\''" }
+    end
   end
 end
