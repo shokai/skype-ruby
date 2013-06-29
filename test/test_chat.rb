45cbe1ed9855603c769e849eeb9cc1198fcdb2d1
Sho Hashimoto <hashimoto@shokai.org>
2013-06-30 01:26:08 +0900

    move chat's test into test_chat.rb

---
+++ test/test_chat.rb
@@ -0,0 +1,31 @@
+require File.expand_path 'test_helper', File.dirname(__FILE__)
+
+class TestSkype < MiniTest::Test
+
+  SKYPE_FROM = ENV["SKYPE_FROM"]
+  SKYPE_TO   = ENV["SKYPE_TO"] || "echo123"
+
+  def test_exec
+    body = "hello exec"
+    msg = Skype.exec "MESSAGE #{SKYPE_TO} #{body}"
+    assert_equal msg.body, body
+    assert_equal msg.time.class, Time
+    assert_equal msg.user, SKYPE_FROM
+  end
+
+  def test_message
+    body = "hello hello"
+    msg = Skype.message SKYPE_TO, body
+    assert_equal msg.body, body
+    assert_equal msg.time.class, Time
+    assert_equal msg.user, SKYPE_FROM
+  end
+
+  def test_message_escape
+    body = "hello \"'$@&()^![]{};*?<>`\\ world"
+    msg = Skype.message SKYPE_TO, body
+    assert_equal msg.body, body
+    assert_equal msg.time.class, Time
+    assert_equal msg.user, SKYPE_FROM
+  end
+end
--- test/test_skype.rb
+++ test/test_skype.rb
@@ -2,30 +2,9 @@ require File.expand_path 'test_helper', File.dirname(__FILE__)
 
 class TestSkype < MiniTest::Test
 
-  SKYPE_FROM = ENV["SKYPE_FROM"]
-  SKYPE_TO   = ENV["SKYPE_TO"]
-
-  def test_exec
-    body = "hello exec"
-    msg = Skype.exec "MESSAGE #{SKYPE_TO} #{body}"
-    assert_equal msg.body, body
-    assert_equal msg.time.class, Time
-    assert_equal msg.user, SKYPE_FROM
+  def test_username
+    res = Skype.exec "GET USER echo123 FULLNAME"
+    assert res =~ /Sound Test Service/i, "API response error"
   end
 
-  def test_message
-    body = "hello hello"
-    msg = Skype.message SKYPE_TO, body
-    assert_equal msg.body, body
-    assert_equal msg.time.class, Time
-    assert_equal msg.user, SKYPE_FROM
-  end
-
-  def test_message_escape
-    body = "hello \"'$@&()^![]{};*?<>`\\ world"
-    msg = Skype.message SKYPE_TO, body
-    assert_equal msg.body, body
-    assert_equal msg.time.class, Time
-    assert_equal msg.user, SKYPE_FROM
-  end
 end
