edda3051c9f249ce4199789c64a050c76a75c1db
Sho Hashimoto <hashimoto@shokai.org>
2013-06-29 14:53:22 +0900

    test Skype.exec #6

--- test/test_skype.rb
+++ test/test_skype.rb
@@ -5,6 +5,18 @@ class TestSkype < MiniTest::Test
   SKYPE_FROM = ENV["SKYPE_FROM"]
   SKYPE_TO   = ENV["SKYPE_TO"]
 
+  def test_exec
+    body = "hello exec"
+    res = Skype.exec "MESSAGE #{SKYPE_TO} #{body}"
+    assert res =~ /^MESSAGE \d+ STATUS SENDING$/, "Skype API response error"
+    res_id = res.scan(/^MESSAGE (\d+) STATUS SENDING$/)[0][0].to_i
+    msg = Skype::Chat::Message.new res_id
+    assert_equal msg.id, res_id
+    assert_equal msg.body, body
+    assert_equal msg.time.class, Time
+    assert_equal msg.user, SKYPE_FROM
+  end
+
   def test_message
     body = "hello hello"
     res = Skype.message SKYPE_TO, body
