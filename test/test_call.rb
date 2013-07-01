d825a1ddb38904b684ce5a7888be9952dea6c22a
Sho Hashimoto <hashimoto@shokai.org>
2013-07-01 10:28:25 +0900

    call echo123 in test

--- test/test_call.rb
+++ test/test_call.rb
@@ -3,14 +3,16 @@ require File.expand_path 'test_helper', File.dirname(__FILE__)
 class TestSkypeCall < MiniTest::Test
 
   SKYPE_FROM = ENV["SKYPE_FROM"]
-  SKYPE_TO   = ENV["SKYPE_TO"] || "echo123"
+  SKYPE_TO   = "echo123"
 
   def test_call
     call = Skype.call SKYPE_TO
     STDERR.puts call if call.kind_of? String
     assert_equal call.class, Skype::Call
     assert_equal call.to, SKYPE_TO
-    sleep 1
+    while !call.talking?
+      sleep 2
+    end
     call.hangup
     assert [:finished, :missed, :cancelled].include? call.status
     assert_equal call.talking?, false
